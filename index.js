237153d998c8f25aa179fbe96c9dc2576895fae3
Max <maksimus.pugachev@gmail.com>
2023-01-16 15:10:37 +0500

    Version 0.11

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-let a = 0;
+const TelegramAPI = require('node-telegram-bot-api')
+require('dotenv').config();
+
+
+const tgBotToken = process.env.TG_TOKEN
+const bot = new TelegramAPI(tgBotToken, { polling: true })
+bot.on('message', msg => {
+    const text = msg.text;
+    const chatID = msg.chat.id;
+    bot.sendMessage(chatID, "Start")
+})
